480b36701871fe2b0077d55048468942a3363903
Mahdi Naji <mahdinaji754@gmail.com>
2024-12-17 10:50:10 +0330

    Create Untitled5.cpp

---
+++ Untitled5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+main()
+{
+	int a = 1000;
+	int b = 2000;
+	int sum = 0;
+	int i;
+	for ( i = a ; i <= b ; i++ )
+	{
+		if ( i % 2 == 0)
+		{
+			
+		sum += a;
+		
+		cout<<i<<endl;
+	} 	}
+	cout<<"Sum is : "<<sum;
+}
